185dab36352cb0b74dd88ce8ed76f3b6c9ffb61f
IWillWebDev <willighn469@gmail.com>
2024-08-14 16:41:19 -0300

    update

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="Banner principal da pagina do Organo"/>
+        </header>      
+    )
+}
+
+export default Banner
