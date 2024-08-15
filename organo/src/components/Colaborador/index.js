dc09bae67c2c18e42dd12f9c201057f06f767767
IWillWebDev <willighn469@gmail.com>
2024-08-14 22:32:34 -0300

    update

---
+++ organo/src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/willihumphreys.png" alt="Willi Humphreys"/>
+            </div>
+            <div className="rodape">
+                <h4>Willi</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
